bfe25851fb62d92787acaba4b33707ec82b26853
Taleb Elmehdi <talebelmehdi1@gmail.com>
2018-06-16 19:24:54 +0000

    My project Setup completed successful

---
+++ restaurantbackend/src/main/java/net/ensa/restaurantbackend/App.java
@@ -0,0 +1,13 @@
+package net.ensa.restaurantbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
